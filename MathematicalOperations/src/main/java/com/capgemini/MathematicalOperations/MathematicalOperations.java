0fad4661d9189ccbf5a04adadea4ce0907202a6b
ADyszy <ADYSZY@CE11704.corp.capgemini.com>
2015-07-22 15:00:17 +0200

    Multiply operation added and tested + Class testing Guava Lib added.

--- MathematicalOperations/src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
+++ MathematicalOperations/src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
@@ -1,5 +1,12 @@
 package com.capgemini.MathematicalOperations;
 
 public class MathematicalOperations {
+	
+	public static long multiply(long ... number) {
+		long result = 1;
+		for (long l : number)
+			result *= l;
+		return result;
+	}
 
 }
