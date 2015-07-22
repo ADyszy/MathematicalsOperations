38c83fbbc601b156cad339b6f74f6a0c0e038e7b
unknown <MKORDULS@CE11687.corp.capgemini.com>
2015-07-22 16:03:12 +0200

    my method using Jsoup library added

---
+++ MathematicalOperations/src/main/java/com/capgemini/MathematicalOperations/JsoupLibrary.java
@@ -0,0 +1,23 @@
+package com.capgemini.MathematicalOperations;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class JsoupLibrary {
+	public String getWebsiteTitle(String url) {
+
+		Document doc = null;
+		try {
+
+			// get http protocol
+			doc = Jsoup.connect(url).get();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return doc.title();
+	}
+
+}
