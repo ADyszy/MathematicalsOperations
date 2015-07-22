cc8acbd7eb84f3e6f004b09bcba8753a9752c67c
Tomasz Zablocki <tomasz.zablocki@capgemini.com>
2015-07-22 16:12:05 +0200

    I've added method adding and tests to it.

--- MathematicalOperations/src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
+++ MathematicalOperations/src/main/java/com/capgemini/MathematicalOperations/MathematicalOperations.java
@@ -1,34 +1,38 @@
 package com.capgemini.MathematicalOperations;
 
 public class MathematicalOperations {
-	
-	public static long multiply(long ... number) {
+
+	public static long multiply(long... number) {
 		long result = 1;
 		for (long l : number)
 			result *= l;
 		return result;
 	}
 
-	public static long substract(long minuend, long subtrahend){
+	public static long substract(long minuend, long subtrahend) {
 		return minuend - subtrahend;
 	}
 
 	public static long factorial(int n) {
-		if(n >= 0){
+		if (n >= 0) {
 			long factorial = 1;
-			
-			for(int i = 1; i <= n; i++){
+
+			for (int i = 1; i <= n; i++) {
 				factorial *= i;
 			}
 			return factorial;
 		}
 		throw new IllegalArgumentException("n must be greater than or equal to 0");
 	}
-	
-	public static double divide (double dividend, double divisor){
-		if(divisor!=0){
-			return dividend/divisor;
+
+	public static double divide(double dividend, double divisor) {
+		if (divisor != 0) {
+			return dividend / divisor;
 		}
 		throw new ArithmeticException("Divisor can't be zero");
+	}
+
+	public static double adding(double number1, double number2) {
+		return number1 + number2;
 	}
 }
